bdb557d9402e4c32195d76eea6b89451a8d15c39
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2014-08-18 10:33:54 +0200

    Changed the pack function to be configurable

--- rndphrase.js
+++ rndphrase.js
@@ -88,15 +88,28 @@
         };
     }
 
-    // pack :: String[64] -> String[16]
-    function pack(msg) {
-        // Note: modulus introduces a bias
-        // use 2 bytes to pick the letter to relax this
-        var s = '',
-            i;
-        for (i = 0; i < msg.length; i += 4) {
-            s += alphabet.charAt(parseInt(msg.substring(i, i + 4), 16) % alphabet.length);
+    function is_alpha(c) {
+        var cc = c.charCodeAt(0);
+        return ((cc > 64 && 91 > cc) || (cc > 96 && 123 > cc));
+    }
+
+    function is_num(c) {
+        return (47 < c.charCodeAt(0) < 58);
+    }
+
+    function str2ints(str) {
+        var ints = [];
+        for (i = 0; i < str.length; i += 2) {
+            ints.push(parseInt(str.substring(i, i + 2), 16));
         }
+        return ints;
+    }
+
+    function setup_source(source, alphabet) {
+        s = source || {};
+        if(!s.min) s.min = 0;
+        if(!s.max) s.max = -1;
+        if(!s.alphabet) s.alphabet = alphabet;
         return s;
     }
 
@@ -131,12 +144,93 @@
 
         version = config.version
 
-        if(!version || version < 0) version = 1; 
+        if(!version || version < 0) {
+            version = 1;
+        }
+
+        size = config.size;
+        if(size == undefined) {
+            size = 16;
+        }
+
+        capital = config.capital;
+        if(capital !== false) {
+            capital = setup_source(capital, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        }
+
+        minuscule = config.minuscule;
+        if(minuscule !== false) {
+            minuscule = setup_source(minuscule, 'abcdefghijklmnopqrstuvwxyz');
+        }
+
+        numeric = config.numeric;
+        if(numeric !== false) {
+            numeric = setup_source(numeric, '0123456789');
+        }
+
+        special = config.special;
+        if(special !== false) {
+            special = setup_source(special, '!@#$%^&*[{()}]_+-= ,.?');
+        }
+
+        self._validate = function(hash, sources) {
+            for(var s in sources) {
+                if(sources[s].min > 0) {
+                    if(sources[s].count < sources[s].min) {
+                        return false;
+                    }
+                }
+            }
+            return hash.length >= size;
+        }
+
+        self.pack = function(unpacked) {
+            //unpacked should be of the size
+            //sources*2*size
+            var sources = [];
+            if(capital) sources.push(capital);
+            if(minuscule) sources.push(minuscule);
+            if(numeric) sources.push(numeric);
+            if(special) sources.push(special);
+
+            if(!sources.length) {
+                throw new Error("RndPhrase: Could not generate valid hash.")
+            }
+
+            //if max for each source is zero, we remove it
+            for(var i = 0; i < sources.length; i++) {
+                if(sources[i].max == 0) {
+                    sources.splice(i,1);
+                } else {
+                    sources[i].count = 0;
+                }
+            }
+
+            var ints = str2ints(unpacked);
+            var hash = '';
+
+            while(!self._validate(hash, sources)) {
+                try {
+                    var integer = ints.shift();
+                    choice = integer % sources.length;
+                    source = sources[choice];
+                    hash += source.alphabet.charAt(ints.shift() % source.alphabet.length);
+                    sources[choice].count++;
+                    if((sources[choice].max > 0) && !(sources[choice].count < sources[choice].max)) {
+                        sources.splice(choice, 1);
+                    }
+                } catch(e) {
+                    //This is most likely due to our bag of ints running short of numbers.
+                    throw new Error("RndPhrase: Could not generate valid hash.");
+                }
+            }
+            return hash;
+        }
 
         self.generator = function (passwd) {
             // produce secure hash from seed, password and host
             return function() {
-                passwd = pack(hash(hash(hash(passwd + '$' + host) + seed) + passwd));
+                passwd = self.pack(hash(hash(hash(passwd + '$' + host) + seed) + passwd));
                 return passwd;
             }
         };
