17c34f8b94d7fae3565b6def54f58f382626c7a2
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2014-10-19 13:10:04 +0200

    Reworked the algorithm for various improvements and simplicity

--- rndphrase.js
+++ rndphrase.js
@@ -31,10 +31,10 @@
 
     function is_special(c) {
         var cc = c.charCodeAt(0);
-        return (31 > cc && cc < 48 ||
-                cc > 57 && cc < 65 ||
-                cc > 90 && cc < 97 ||
-                cc > 122 && cc < 127);
+        return ((31 < cc && cc < 48) ||
+                (cc > 57 && cc < 65) ||
+                (cc > 90 && cc < 97) ||
+                (cc > 122 && cc < 127));
     }
 
    function setup_source(source, alphabet) {
@@ -43,19 +43,17 @@
         if(isNaN(min)) s.min = 1;
         var max = parseInt(s.max);
         if(isNaN(s.max)) s.max = 0;
-        if(!s.alphabet) s.alphabet = alphabet;
+        // if(!s.alphabet) s.alphabet = alphabet;
         return s;
     }
 
-    function validate(hash, sources, size) {
-        for(var s in sources) {
-            if(sources[s].min > 0) {
-                if(sources[s].count < sources[s].min) {
-                    return false;
-                }
-            }
+    function validate(h, rules, size) {
+        for(var r in rules) {
+            var rule = rules[r];
+            if(rule.count < rule.min) return false;
         }
-        return hash.length >= size;
+
+        return h.length >= size;
     }
 
     function RndPhrase(config) {
@@ -87,37 +85,86 @@
             self.size = 16;
         }
 
+        self.rules = {};
+
         self.capital = config.capital;
-        // if(self.capital !== false) {
-        //     self.capital = setup_source(self.capital, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-        // }
+        if(config.capital !== false) {
+            var alpha = '';
+            var cfg = config.capital || {};
+
+            for(var cc = 65; cc < 91; cc++) {
+                alpha += String.fromCharCode(cc);
+            }
+            
+            self.rules.capital = {
+                'min': cfg.min || 1,
+                'max': cfg.max || 0,
+                'alphabet': cfg.alphabet || alpha
+            };
+        }
 
         self.minuscule = config.minuscule;
-        // if(self.minuscule !== false) {
-        //     self.minuscule = setup_source(self.minuscule, 'abcdefghijklmnopqrstuvwxyz');
-        // }
+        if(self.minuscule !== false) {
+            var alpha = '';
+            var cfg = config.minuscule || {};
+            for(var cc = 97; cc < 123; cc++) {
+                alpha += String.fromCharCode(cc);
+            }
+            
+            self.rules.minuscule = {
+                'min': cfg.min || 1,
+                'max': cfg.max || 0,
+                'alphabet': cfg.alphabet || alpha
+            };
+        }
 
         self.numeric = config.numeric;
-        // if(self.numeric !== false) {
-        //     self.numeric = setup_source(self.numeric, '0123456789');
-        // }
+        if(self.numeric !== false) {
+            var alpha = '';
+            var cfg = config.numeric || {};
+            for(var cc = 48; cc < 58; cc++) {
+                alpha += String.fromCharCode(cc);
+            }
+            self.rules.numeric = {
+                'min': cfg.min || 1,
+                'max': cfg.max || 0,
+                'alphabet': cfg.alphabet || alpha
+            };
+        }
 
         self.special = config.special;
-        // if(self.special !== false) {
-        //     self.special = setup_source(self.special, " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
-        // }
-
-        self.alphabet = config.alphabet;
-        if(!self.alphabet) {
-            self.alphabet = [];
-            for(var i = 32; i < 127; i++) {
-                self.alphabet.push(String.fromCharCode(i));
+        if(self.special !== false) {
+            var cc = 32;
+            var alpha = '';
+            var cfg = config.special || {};
+            while(cc < 127) {
+                if(cc < 48 || (cc > 57 && cc < 65) ||
+                   (cc > 90 && cc < 97) || cc > 122) {
+                    alpha += String.fromCharCode(cc);
+                }
+                cc++;
             }
-        } else {
-            self.alphabet.split('').sort();
+            self.rules.special = {
+                'min': cfg.min || 1,
+                'max': cfg.max || 0,
+                'alphabet': cfg.alphabet || alpha
+            };
+        }
+
+        self.createAlphabet = function(rules) {
+            var alpha = ''
+            
+            for(var r in rules) {
+                alpha += rules[r].alphabet
+            }
+            
+            //this requires Javascript 1.6
+            return alpha.split('').filter(function(v, i, s) {
+                return s.indexOf(v) === i;
+            }).sort();
         }
         
-        
+
         self.pack = function(unpacked) {
 
             function getInt(size) {
@@ -125,51 +172,68 @@
                     //maybe we can also use the alphabets or something for adding more entropy
                     unpacked += hash(hash(hash(tmp) + unpacked) + size);
                 }
-
                 var n = parseInt(unpacked.substring(0,size), 16);
                 unpacked = unpacked.substring(size);
                 return n;
             }
 
             var tmp = '';
-            var divisor = self.alphabet.length;
+            var metadata = self.rules;
+            var min_size = 0;
+            var alpha = '';
+            for(var k in metadata) {
+                var md = metadata[k];
+                metadata[k].count = 0;
+                if(md.max < md.min) {
+                    min_size += self.size;
+                } else {
+                    min_size += m.max;
+                }
+            }
+            var alphabet = self.createAlphabet(metadata);
+            var divisor = alphabet.length;
 
-            //Figure out how big ints we need
-            //we calculate it here for consistency
+            //How large should the ints be?
+            //Put here so we always pick the same size consistently
             var n = 0;
             while(Math.pow(16, n) < divisor) n++;
             var m = Math.pow(16, n);
 
-
-            //while(!validate(tmp, sources, Math.min(min_size, self.size))) {
-            while(tmp.length <= self.size) {
+            while(!validate(tmp, metadata, Math.min(min_size, self.size))) {
                 try {
                     var idx;
                     var c;
-
+                    var r;
+                    
                     do {
                         idx = getInt(n);
                     } while(idx >= m - (m % divisor));
 
-                    c = self.alphabet[idx % divisor];
+                    c = alphabet[idx % divisor];
+                    
+                    if(is_capital(c)) {
+                        r = 'capital';
+                    } else if(is_minuscule(c)) {
+                        r = 'minuscule';
+                    } else if(is_numeric(c)) {
+                        r = 'numeric';
+                    } else if(is_special(c)) {
+                        r = 'special';
+                    } else {
+                        //non-ascii char
+                        throw new Error("Illegal character: " + c);
+                    }
+
+                    if(metadata[r].max >= metadata[r].min && metadata[r].count >= metadata[r].max) {
+                        delete metadata[r];
+                        alphabet = self.createAlphabet(metadata);
+                        divisor = alphabet.length;
+                        continue;
+                    }
                     tmp += c;
-                    //c = c % divisor; //cap to full alphabet length
-
-                    /*var choice = 0;
-                    while(c >= sources[choice].alphabet.length) {
-                        c -= sources[choice].alphabet.length;
-                        choice++;
-                    }*/
-
-                    /*var source = sources[choice];
-                    tmp += source.alphabet.charAt(c);
-                    sources[choice].count++;
-                    if(source.max >= source.min && sources[choice].count >= source.max) {
-                        divisor -= source.alphabet.length;
-                        sources.splice(choice, 1);
-                    }*/
+                    metadata[r].count++;
                 } catch(e) {
-                    //This is most likely due to our bag of ints running short of numbers.
+                    console.log(e);
                     throw new Error("RndPhrase: Could not generate valid hash.");
                 }
             }
